5a4634c3f59637da2f7a5e3c2f2349be8558044d
sunlet <liuhang.163@163.com>
2020-07-23 14:25:59 +0800

    commit

--- testapp/src/test/java/test/jplugin/extension/testapp/App.java
+++ testapp/src/test/java/test/jplugin/extension/testapp/App.java
@@ -11,5 +11,10 @@ public class App
     public static void main( String[] args )
     {
         PluginApp.main(null);
+        System.out.println("cbqcbq==========");
+        
+        System.out.println("cbqcbq==========2222");
+        
+        
     }
 }
